2afd86e54ec44d55fa0e446e3f2285dc6e563d22
marth8880 <marth8880@gmail.com>
2017-09-10 11:53:57 -0700

    First pass functionality

---
+++ BuildNumGen/BuildNumGen.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace BuildNumGen
+{
+    partial class BuildNumGen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Generate = new System.Windows.Forms.Button();
+            this.lbl_BuildNum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Generate
+            // 
+            this.btn_Generate.Location = new System.Drawing.Point(12, 68);
+            this.btn_Generate.Name = "btn_Generate";
+            this.btn_Generate.Size = new System.Drawing.Size(157, 23);
+            this.btn_Generate.TabIndex = 0;
+            this.btn_Generate.Text = "Generate";
+            this.btn_Generate.UseVisualStyleBackColor = true;
+            this.btn_Generate.Click += new System.EventHandler(this.btn_Generate_Click);
+            // 
+            // lbl_BuildNum
+            // 
+            this.lbl_BuildNum.AutoSize = true;
+            this.lbl_BuildNum.Location = new System.Drawing.Point(12, 27);
+            this.lbl_BuildNum.Name = "lbl_BuildNum";
+            this.lbl_BuildNum.Size = new System.Drawing.Size(35, 13);
+            this.lbl_BuildNum.TabIndex = 1;
+            this.lbl_BuildNum.Text = "label1";
+            // 
+            // BuildNumGen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(181, 103);
+            this.Controls.Add(this.lbl_BuildNum);
+            this.Controls.Add(this.btn_Generate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "BuildNumGen";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Build Number Generator";
+            this.Load += new System.EventHandler(this.BuildNumGen_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Generate;
+        private System.Windows.Forms.Label lbl_BuildNum;
+    }
+}
+
---
+++ BuildNumGen/BuildNumGen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuildNumGen
+{
+    public partial class BuildNumGen : Form
+    {
+        public BuildNumGen()
+        {
+            InitializeComponent();
+        }
+
+        public string buildNum;
+
+        private void BuildNumGen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Generate_Click(object sender, EventArgs e)
+        {
+            DateTime date = DateTime.Now;
+            string month = date.Month.ToString("00");
+            string day = date.Day.ToString("00");
+            string releaseNum = Properties.Settings.Default.ReleaseNum.ToString("00");
+            string yearsSinceRelease = Properties.Settings.Default.YearsSinceRelease.ToString();
+
+            if (yearsSinceRelease == "0")
+            {
+                yearsSinceRelease = "";
+            }
+
+            buildNum = yearsSinceRelease + month + day + "/" + releaseNum;
+
+            lbl_BuildNum.Text = buildNum;
+        }
+    }
+}
